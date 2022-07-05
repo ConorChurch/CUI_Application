81c2ef412a43a88cc242bca4e2d9739b1c85ca40
Conor Church <conorchurch42@gmail.com>
2022-07-05 18:00:43 +0100

    Updating CSS to show long messages and to bring index.js up to date with react 18

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react';
 
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
-
-ReactDOM.render(
-  <React.StrictMode>
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
