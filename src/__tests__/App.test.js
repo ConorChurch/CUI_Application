b5d23b8fc8fa071a84b214cb306ea812ece10513
Conor Church <conorchurch42@gmail.com>
2022-12-27 01:19:08 +0000

    Creating tests for frontend components

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,19 +1,64 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from '../App.js';
+import ReactDOM from 'react-dom/client';
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+import UserConvo from '../components/UserConvo';
+import renderer from 'react-test-renderer';
+import TextBox from '../components/TextBox';
+import EndPage from '../components/EndPage';
+import Buttons from '../components/Buttons';
 
-globalThis.IS_REACT_ACT_ENVIRONMENT = true;
 
-
-it('renders without crashing', () => {
-  const container = document.getElementById('root');
-  const root = createRoot(container); // createRoot(container!) if you use TypeScript
-  root.render(<App />);
+test("renders App.js", () => {
+    render(<App />);
 });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders App.js", () => {
+  render(<UserConvo />);
 });
+
+// Last test should be maybe if there is an input into the user conversation, it should render that
+// in the UserConvo component 
+// Integration or full test through(forget name)? 
+
+
+it('renders snapshot of the User conversation', () => {
+  const component = renderer.create(
+    <UserConvo />,
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+  });
+
+  test("renders TextBox.jsx", () => {
+    render(<TextBox />);
+  });
+
+  test("renders EndPage.jsx", () => {
+    const endMessage = "Thanks for chatting!";
+    render(<EndPage endMessage={endMessage}/>);
+  });
+
+  test("renders EndPage.jsx", () => {
+    const endMessage = "Thanks for chatting!";
+    const component = renderer.create(<EndPage endMessage={endMessage}/>);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test("renders Buttons.jsx", () => {
+    const choices = ["Button 1", "Button 2"]
+    render(<Buttons choices={choices}/>);
+  });
+
+
+  it('renders snapshot of the Button options', () => {
+    const choices = ["Button 1", "Button 2"]
+    const component = renderer.create(
+      <Buttons choices={choices}/>,
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+    });
+
+  
+
